afbb330d2c4e68c51e3de8ff232889ebd1795e03
Ilya Yalymov <chezeee@gmail.com>
2023-01-30 20:48:47 +0300

    Initial structure added

---
+++ js/bill.js
@@ -0,0 +1,12 @@
+export default class Bill {
+  constructor() {
+    this.amounts = [];
+  }
+
+  /** @param {string} amountStr */
+  addAmount(amountStr) {
+    // your code
+  }
+
+  // your code
+}
